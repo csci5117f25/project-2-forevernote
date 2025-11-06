605f50fc06c7f7e735e919947f2973353da5fd66
Yaseen Shakil <yaseenshakil@infra04-wg312.eduroam.wireless.umn.edu>
2025-11-06 16:47:29 -0600

    Splash page changes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import imgSrc from '../assets/images/Logo.png'
+</script>
+
+<template>
+    <nav class="navbar is-white">
+        <div class="navbar-brand">
+            <img :src="imgSrc" alt="Logo" id="logo" />
+        </div>
+        <div class="navbar-end">
+            <div class="navbar-item">
+            <button class="button is-primary is-rounded">Get Started</button>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+#logo {
+  max-height: 3.5rem; 
+  width: auto;
+  height: auto;
+  object-fit: contain;
+}
+
+.navbar{
+    border-bottom: 1pt solid gray;
+}
+</style>
---
+++ src/components/SplashWelcome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="columns" id="splash-page-container">
+        <div class="column splash-tagline">
+            <h1>We make notes better...forever</h1>
+        </div>
+        <div class="column splash-img">
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#splash-page-container{
+    margin: 2% 2%;
+    margin-right: 2%;
+    width: 96%;
+}
+.splash-tagline{
+    font-size: 6.5rem;
+    color: black;
+
+}
+
+.splash-img{
+    border: 2px solid black
+}
+</style>
