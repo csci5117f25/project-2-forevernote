c6325f6dafcf47599fc58fd924be40d5066a3e26
2025linw <2025linw@gmail.com>
2025-12-02 10:16:25 -0600

    remove compiler macro defineEmits

--- src/components/NewExamModal.vue
+++ src/components/NewExamModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
 import { useCurrentUser, useFirestore } from 'vuefire';
 import { collection, addDoc } from 'firebase/firestore';
 
