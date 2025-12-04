918ba389c2fa94084dd9a28817d955a9626e5348
2025linw <2025linw@gmail.com>
2025-12-04 15:20:06 -0600

    added icons for later use

---
+++ src/components/icons/IconTrashFill.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import IconBase from './IconBase.vue';
+
+const props = defineProps({
+  size: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <IconBase :color="props.color" :size="props.size">
+    <path
+      d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+  </IconBase>
+</template>
