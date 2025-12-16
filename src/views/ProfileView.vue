2a6d2980c84eaf7e5bb40932c8227bbbbc22186e
joshua Cheng <chengjoshua22@gmail.com>
2025-12-16 15:51:04 -0600

    basic profile page

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,10 +1,35 @@
 <script setup>
+import { ref, computed, watch } from 'vue';
 import { useRouter } from 'vue-router';
-import { useFirebaseAuth } from 'vuefire';
+import { useCollection, useFirebaseAuth, useCurrentUser, useFirestore } from 'vuefire';
 import { signOut } from 'firebase/auth';
+import { collection } from 'firebase/firestore';
 
 const auth = useFirebaseAuth();
 const router = useRouter();
+
+const db = useFirestore();
+
+const user = useCurrentUser();
+
+const notesRef = collection(db, 'users', user.value?.uid || 'no-user', 'notes');
+const notes = useCollection(notesRef);
+
+watch(
+  notes,
+  (newNotes) => {
+    if (!newNotes) return;
+    newNotes.forEach((n) => {
+      if (n && n.isSelected === undefined) { n.isSelected = false; };
+    });
+  },
+  { immediate: true, deep: true }
+);
+
+const countNotes = computed(() => {
+  return notes.value ? notes.value.length : 0;
+});
+
 
 async function logout() {
   try {
@@ -18,7 +43,8 @@ async function logout() {
 </script>
 
 <template>
-  <p class="title has-text-dark">Profile View</p>
+  <p class="title has-text-dark">Hey {{ user.displayName }}!</p>
+  <p class="has-text-dark">notes created: {{ countNotes }}</p>
 
   <div class="field is-grouped is-grouped-center">
     <button class="button" @click="logout">Logout</button>
